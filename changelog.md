c86f7a5dac3eef89b21701962887e15cfac48113
joaovcalvet <joaovictor.calvet@outlook.com>
2026-07-15 00:27:46 -0300

    [feat]: added users table conceptual model

---
+++ changelog.md
@@ -0,0 +1,3 @@
+# 0.0.1
+- Adicionado o modelo conceitual e lógico da tabela "users"
+- Adicionado log ao iniciar o servidor
--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,11 @@ import express, { type Express, type Request, type Response } from 'express';
 const port: Number = 3000;
 const app: Express = express();
 
+// Rota de Teste
 app.get('/', (req: Request, res: Response) => {
     res.send("Hello, world!");
 });
 
-app.listen(port);
+app.listen(port, () => {
+    console.log(`Aplicação rodando na porta: ${port}`)
+});
